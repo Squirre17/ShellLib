830de419a4115e2ae5b42fc5d11b6d08a2b7f36e
squ <3319277663@qq.com>
2022-07-10 22:08:46 +0800

    modify some files

--- git_quick_push.sh
+++ git_quick_push.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # integrate "add" "commit" and "push" all in one
+# WARN: make sure have used "git push -u origin master" cmd 
 set -e
 git add .
 git commit -m "$1"
-git push -u origin master
+git push
